39eb87d4a04ec8aa3ff2869de6fbd8c9c7426dd6
Matthew Routley <matt@routleynet.org>
2015-07-06 17:16:33 -0400

    Extend case study and add Rmd

--- presentation/example.R
+++ presentation/example.R
@@ -7,5 +7,16 @@ rand <- rnorm(1000)
 
 # Many built in functions
 hist(rand)
+
 # Get help with ?
 ?hist
+
+# Linear model ------------------------------------------------------------
+
+x <- rnorm(1000, mean = 100)
+y <- rnorm(1000, mean = 700)
+plot(x, y)
+model <- lm(y ~ x)
+summary(model)
+# Add the line of best fit
+abline(model, col = "red")
